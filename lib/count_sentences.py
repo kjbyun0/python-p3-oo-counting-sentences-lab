7a3da0bffa7707d8ed8c831029ea500e1f8e6cd6
KI JONG BYUN <kjbyun0@gmail.com>
2024-01-15 03:59:59 -0600

    Assignment Done

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,42 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value=''):
+    self.value = value
+
+  @property
+  def value(self):
+    return self._value
+  
+  @value.setter
+  def value(self, value):
+    if isinstance(value, str):
+      self._value = value
+    else:
+      print('The value must be a string.')
+
+  def is_end_with(self, ch):
+    if self.value[-1] == ch:
+      return True
+    return False
+
+  def is_sentence(self):
+    return self.is_end_with('.')
+  
+  def is_question(self):
+    return self.is_end_with('?')
+  
+  def is_exclamation(self):
+    return self.is_end_with('!')
+  
+  def count_sentences(self):
+    tmp = self.value.replace('?', '.')
+    tmp = tmp.replace('!', '.')
+    sentences = tmp.split('.')
+    return len([sentence for sentence in sentences if sentence != ''])
+
+  
+
+  
+
+
